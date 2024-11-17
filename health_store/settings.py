f048c41a440c16bc675bb8c5398f9d0bbd4676fc
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-11-17 14:38:13 +0000

    Create the home app with its basic setup

--- health_store/settings.py
+++ health_store/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     # Project apps
+    'home',
     'profiles',
     # Other
     'django_countries',
--- health_store/urls.py
+++ health_store/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('profile/', include('profiles.urls')),
+    path('', include('home.urls')),
 ]
