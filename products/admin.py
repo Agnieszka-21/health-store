7f232a57be298aadc1c39f59ffd5858ce9bea7ff
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-11-22 17:28:40 +0000

    Add image model, create a template and view for all products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Product, Category, Image
+
 
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Image)
--- products/views.py
+++ products/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category, Product
 
 # Create your views here.
+
 def all_products(request):
-    return HttpResponse('This is where all products live :)')
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
+    # query = None
+    # categories = None
+    # sort = None
+    # direction = None
+
+    # current_sorting = f'{sort}_{direction}'
+
+    context = {
+        'products': products,
+        # 'search_term': query,
+        # 'current_categories': categories,
+        # 'current_sorting': current_sorting,
+    }
+
+    return render(request, 'products/products.html', context)
