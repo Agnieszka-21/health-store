4611c6ab0b66cbf518014cfd228102029b755dbb
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-12-02 10:55:25 +0000

    Add toasts in the base template (functionality and basic styling)

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_basket(request):
@@ -8,7 +11,7 @@ def view_basket(request):
 
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
-
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -17,6 +20,8 @@ def add_to_basket(request, item_id):
         basket[item_id] += quantity
     else:
         basket[item_id] = quantity
+        print(f'Added {product.name} to your basket')
+        messages.success(request, f'Added {product.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
