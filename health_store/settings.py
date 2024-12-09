4c8c0d449df99ea8b40a02181b57a8f70670de69
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-12-09 10:22:44 +0000

    Install gunicorn, set up databases, prepare the project for deployment on Heroku

--- health_store/settings.py
+++ health_store/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+
 import os
+import dj_database_url
 
 if os.path.isfile('env.py'):
     import env
@@ -27,10 +29,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-agnieszka21-healthstore-gnje3zewcam.ws.codeinstitute-ide.net',
+    'localhost',
+    'health-store-ff0f909bba3d.herokuapp.com/',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -121,12 +125,17 @@ WSGI_APPLICATION = 'health_store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
