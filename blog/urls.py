4146e35d1fe9b2922c8ff56dae890ccd9c340feb
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-12-13 18:39:11 +0000

    Fix the summernote url

--- blog/urls.py
+++ blog/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
     path('recipes/', views.RecipeListView.as_view(), name='recipes'),
     path('recipes/<slug:slug>/', views.recipe_detail, name='recipe_detail'),
-    path('summernote/', include('django_summernote.urls')),
     path('', views.choose_articles_or_recipes, name='blog_options'),
 ]
