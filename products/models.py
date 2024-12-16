c8256d2748859ac8ecf28d607aa37fc4b644babd
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-12-16 12:01:44 +0000

    Add alphabetic ordering for products by their name

--- products/models.py
+++ products/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
-
-    name = models.CharField(max_length=254)
-    friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -17,7 +16,6 @@ class Category(models.Model):
 
 
 class Brand(models.Model):
-
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
     url = models.URLField(max_length=180, null=True, blank=True)
@@ -41,6 +39,9 @@ class Product(models.Model):
     ingredients = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=1, null=True, blank=True)
+
+    class Meta:
+        ordering = ['name']
 
     def __str__(self):
         return self.name
