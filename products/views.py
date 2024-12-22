18672534dc9cab28a04ca18517e3c8aab76d227f
Agnieszka Bialek <agnieszkabialekmusic@gmail.com>
2024-12-22 15:40:36 +0000

    Sort products on the shop page by price, rating, and name

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models.functions import Lower
 
 from .models import Category, Brand, Product, Image, Wishlist
 from .forms import ProductForm, ImageForm
@@ -18,11 +19,24 @@ def all_products(request):
     categories = None
     brands = None
         
-    # sort = None
-    # direction = None
-    # current_sorting = f'{sort}_{direction}'
+    sort = None
+    direction = None
 
     if request.method == "GET":
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -46,6 +60,8 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(ingredients__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'all_brands': all_brands,
@@ -53,7 +69,7 @@ def all_products(request):
         'all_categories': all_categories,
         'current_categories': categories,
         'search_term': query,
-        # 'current_sorting': current_sorting,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
